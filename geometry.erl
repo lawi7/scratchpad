ce57d7254918cf817b5e845af0bb4be327049856
Lawrence Ochieng <Lawrence.ochieng@refinitiv.com>
2020-09-26 19:28:13 +0100

    recursion 1

--- geometry.erl
+++ geometry.erl
@@ -1,6 +1,12 @@
 -module(geometry).
 -export([area/1]).
 
+test() -> 
+    12 = area({rectangle, 3, 4}),
+    144 = area({square, 12}),
+    tests_worked.
+
 area({rectangle, Width, Height}) -> Width * Height;
-area({square, Side})		 -> Side * Side.
+area({square, Side})		     -> Side * Side;
+area({circle, Radius})           -> 3.14159 * Radius.
 
